915dc4995ccde433a13d38115a647d42cf572f5d
Robert Wang <rwang5688@outlook.com>
2024-05-21 07:16:44 +0000

    add streamlit run shell scripts

---
+++ workshop_5/amazon-kendra-langchain-extensions/kendra_retriever_samples/run_app_bedrock_claudev3_sonnet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+export AWS_PROFILE="default"
+export AWS_REGION="us-west-2"
+export KENDRA_INDEX_ID="f3774bdc-92c7-4110-a0da-5d6410099213"
+export FALCON_40B_ENDPOINT="jumpstart-dft-hf-llm-falcon-40b-instruct-bf16"
+export LLAMA_2_ENDPOINT="jumpstart-dft-meta-textgeneration-llama-2-70b-f"
+
+export OPENAI_API_KEY="<YOUR-OPEN-AI-API-KEY>"
+export ANTHROPIC_API_KEY="<YOUR-ANTHROPIC-APIKEY>"
+
+# Usage: streamlit run app.py <anthropic|openai|falcon40b|llama2|llama2_n|bedrock_titan|bedrock_claudev2|bedrock_claudev3_haiku|bedrock_claudev3_sonnet|bedrock_llama2_13b|bedrock_llama2_70b>
+streamlit run app.py bedrock_claudev3_sonnet --server.port 8080
---
+++ workshop_5/summarization/run_summarization_app.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Usage: streamlit run summarization_app.py --server.port 8080
+streamlit run summarization_app.py --server.port 8080
